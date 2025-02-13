ddfd41fc4d7d9ca6a6cfa060e373d5925060afb3
NeppDan <JohannJoseph26@gmail.com>
2025-02-13 22:20:26 +0800

    removed forum1.html

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function joinForum() {
     const code = document.getElementById("forumCode").value;
     if (code === "FORUM1") {
-        window.location.href = "forum1.html"; 
+        window.location.href = "forum_post.html"; 
     } else {
         alert("Please enter a forum code!");
     }
