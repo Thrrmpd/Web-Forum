61cc75daa8fd85d09e4e1c2297591b24fd689b42
HannahCorpuz <134398339+HannahCorpuz@users.noreply.github.com>
2025-03-15 02:39:13 +0800

    modified:   public/forum_post.html 	modified:   public/index_userprofile.html 	modified:   public/script_userprofile.js

--- public/script_userprofile.js
+++ public/script_userprofile.js
@@ -42,7 +42,7 @@ async function isLogin(){ //login function
         var userArray = Object.entries(userdata); //get array of objects from userdata
         var forumArray = Object.entries(forumdata); //get array of objects from forumdata
         var info;
-        var forums;
+        var forums = [];
 
         for(var i = 0; i < userArray.length; i++) //for loop to get user with ID == const ID 
         {
@@ -56,22 +56,88 @@ async function isLogin(){ //login function
         for(var i = 0; i < forumArray.length; i++)
         {
             if(ID == Object.values(forumArray[i][1])[5]){
-                forums = Object.values(forumArray[i][1])
+                forums.push(Object.values(forumArray[i][1]))
             }
 
         }
 
-        console.log(forums == true);
-        console.log(Object.values(forumArray[0][1])[5] == ID);
-        console.log(ID);
-        console.log(Object.values(forumArray[0][1])[5]);
 
-        displayInfo(info); //display info
-        //displayForums();
+        displayInfo(info); 
+
+        if(forums.length != 0)
+            displayForums(forums, info[2]);
 
     }catch(err){
         console.error(err);
     }
+
+}
+
+function displayForums(forums, creatorName){
+    const forumEntries = document.getElementById("forum-list"); //get contents of forum-entry class in index_userprofile.html
+    forumEntries.innerHTML = '';
+    var index;
+    console.log(forums);
+
+    
+
+    for(var i = 0; i < forums.length; i++)
+    {
+
+        const newDiv = document.createElement('div');
+        const newSpan = document.createElement('span');
+        const newButton = document.createElement('button');
+        const forumDescription = document.createElement('div');
+
+
+        newDiv.className = 'forum-entry';
+        newSpan.className = 'forum-title';
+        newButton.className = 'info-button';
+
+        
+        newSpan.textContent = forums[i][2];
+        newButton.textContent = '...';
+
+        forumDescription.className = `forum-entry`;
+        forumDescription.setAttribute('name', `forum${i}`);
+        forumDescription.appendChild(document.createTextNode(`Creator: ${creatorName}`));
+        forumDescription.appendChild(document.createElement('br'));
+        forumDescription.appendChild(document.createElement('br'));
+        forumDescription.appendChild(document.createTextNode(`Description: ${forums[i][3]}`));
+        forumDescription.appendChild(document.createElement('br'));
+        forumDescription.appendChild(document.createElement('br'));
+        forumDescription.appendChild(document.createTextNode(`Forum Code: ${forums[i][4]}`));
+        forumDescription.style.display = 'none';
+
+        index = i;
+
+        newButton.addEventListener('click', (function(i){
+            var display = 0;
+            
+            return function(){
+                var show = document.querySelector(`div[name=forum${i}]`);
+
+                if(display == 0){
+                    show.style.display = 'block';                   
+                    display = 1;
+                }
+                else{
+                    show.style.display = 'none';
+                    display = 0;
+                }
+                
+            };
+
+        })(i));
+
+        newDiv.appendChild(newSpan);
+        newDiv.appendChild(newButton);
+        forumEntries.appendChild(newDiv);
+        forumEntries.appendChild(forumDescription);
+
+    }
+
+
 
 }
 
@@ -80,38 +146,12 @@ function displayInfo(info){ //For displaying corresponding username, email, and 
     const displayUserName = document.querySelector('.profile-username h2'); //get contents of h2 in profile-username div class in index_userprofile.html
     const displayEmail = document.querySelector('.profile-username h3'); //get contents of h3 in profile-username div class in index_userprofile.html
     const displayProfile = document.querySelector('.profile-img'); //get contents of img element profile-img class in index_userprofile.html
-    const forumEntries = document.querySelectorAll(".forum-entry"); //get contents of forum-entry class in index_userprofile.html
-    
-    const newDiv = document.createElement('div');
-    const newEntry = document.createElement('span');
-    const newButton = document.createElement('button');
 
-    newDiv.className = 'forum-entry';
-    newEntry.className = 'forum-title';
-    newButton.className = 'info-button';
 
     displayUserName.textContent = info[2]; //display username (ex. "Ava Lee") to index_userprofile.html
     displayEmail.textContent = info[3]; //display email (ex. "hannahcorpuz2003@gmail.com") to index_userprofile.html
     displayProfile.src = info[6]; //display corresponding pfp of user to index_userprofile.html
-
-
     
-}
-
-
-document.addEventListener("DOMContentLoaded", function () {
-    const forumEntries = document.querySelectorAll(".forum-entry");
-
-    forumEntries.forEach(entry => {
-        entry.addEventListener("click", function () {
-            const forumTitle = this.querySelector(".forum-title").textContent;
-            showForumDetails(forumTitle);
-        });
-    });
-});
-
-function showForumDetails(forumTitle) {
-    alert(`Forum Information for ${forumTitle}`);
 }
 
 document.addEventListener("DOMContentLoaded", function () {
