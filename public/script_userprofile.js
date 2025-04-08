349388c83e18061bbe7fb167ac7b8568cac578b3
HannahCorpuz <134398339+HannahCorpuz@users.noreply.github.com>
2025-04-08 19:44:25 +0800

    t

--- public/script_userprofile.js
+++ public/script_userprofile.js
@@ -1,8 +1,11 @@
+const crypto = require('bcrypt');
 var userArray, forumArray, userObject, loggedinUsers, loggedinForums, pathway, loginStatus;
 
 document.addEventListener("DOMContentLoaded", async () => {
     console.log(sessionStorage.getItem('loginID'));
     var forums = [];
+
+    console.log(crypto.hash("helloworld", 10));
 
     if (document.cookie) {
         const userCookie = document.cookie.split(';');
